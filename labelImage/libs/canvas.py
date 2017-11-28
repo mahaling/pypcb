9c8c39cf0149028caf27bdd500efb4c098555bf7
Kevin Gay <12kgay@gmail.com>
2017-11-28 11:22:47 -0500

    Got rid of annoying 'image' tooltip

--- labelImage/libs/canvas.py
+++ labelImage/libs/canvas.py
@@ -194,7 +194,8 @@ class Canvas(QWidget):
         # - Highlight shapes
         # - Highlight vertex
         # Update shape/vertex fill and tooltip value accordingly.
-        self.setToolTip("Image")
+
+        self.setToolTip("")
         for shape in reversed([s for s in self.shapes if self.isVisible(s)]):
             # Look for a nearby vertex to highlight. If that fails,
             # check if we happen to be inside a shape.
