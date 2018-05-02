050becb5f10f249d7506f9725501ccd003180377
Gayathri Mahalingam <gayumahalingam@gmail.com>
2018-05-01 21:37:39 -0700

    removing caffe models

--- setup.py
+++ setup.py
@@ -2,6 +2,20 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 #from setuptools import setup, find_packages
 import os
+import numpy
+
+# download the models first
+icmodel = "wget https://www.dropbox.com/s/14miegbbkr0z4zs/model_ic.caffemodel -O ./detector/models/ic/model_ic.caffemodel"
+capacitormodel = "wget https://www.dropbox.com/s/rgcnigljeg1tzth/model_capacitor.caffemodel -O ./detector/models/capacitor/model_capacitor.caffemodel"
+resistormodel = "wget https://www.dropbox.com/s/0nt5i9ylrvspsic/model_resistor.caffemodel -O ./detector/models/resistor/model_resistor.caffemodel"
+textmodel = "wget https://www.dropbox.com/s/52mlxjog501cm02/ctpn_trained_model.caffemodel -O ./textDetector/models/ctpn_trained_model.caffemodel"
+fontmodel = "wget https://www.dropbox.com/s/tvkjz46wigzc41l/font_model.caffemodel -O ./textDetector/models/font_model.caffemodel"
+
+os.system(icmodel)
+os.system(capacitormodel)
+os.system(resistormodel)
+os.system(textmodel)
+os.system(fontmodel)
 
 datadir = 'detector/models'
 datafiles = [(d, [os.path.join(d,f) for f in files])
@@ -13,7 +27,7 @@ with open('./requirements.txt', 'r') as f:
 cpuNMS = Extension('cpu_nms',
                    sources=['textDetector/cpu_nms.pyx'])
 nmsModule = Extension('cpu_nms',
-                      include_dirs=['/usr/include/python2.7'],
+                      include_dirs=['/usr/include/python2.7', numpy.get_include()],
                       extra_compile_args=['-shared', '-pthread', '-fPIC', '-fwrapv', '-O2', '-Wall', '-fno-strict-aliasing'],
                       sources=['textDetector/cpu_nms.c'])
 
